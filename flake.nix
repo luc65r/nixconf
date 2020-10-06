216eb493fd22c383e75570690386df718ac7b940
Lucas Ransan <lucas@ransan.tk>
2020-10-06 20:09:29 +0200

    Init

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "My NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+  };
+
+  outputs = { self, nixpkgs, home-manager }: {
+    nixosConfigurations = {
+      sally = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.lucas = import ./home.nix;
+            };
+          }
+        ];
+      };
+    };
+  };
+}
