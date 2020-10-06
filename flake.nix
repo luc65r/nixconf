83fa3b6dd2aefe04972c317f494ed0cb02df824a
luc65r <lucas@ransan.tk>
2020-10-06 20:51:58 +0200

    Rearrange layout

--- flake.nix
+++ flake.nix
@@ -11,13 +11,13 @@
       sally = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./configuration.nix
+          ./hosts/sally
           home-manager.nixosModules.home-manager
           {
             home-manager = {
               useGlobalPkgs = true;
               useUserPackages = true;
-              users.lucas = import ./home.nix;
+              users.lucas = import ./home;
             };
           }
         ];
