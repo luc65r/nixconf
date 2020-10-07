95bb7f6d228626f9682c9519db10295e2e2163ef
luc65r <lucas@ransan.tk>
2020-10-07 17:43:02 +0200

    Enable touchpad

--- desktops/default.nix
+++ desktops/default.nix
@@ -6,6 +6,10 @@ desktop:
     enable = true;
     layout = "fr";
     xkbVariant = "bepo";
+
+    libinput = {
+      enable = true;
+    };
   };
 
   home-manager.users.lucas = import ./home.nix desktop;
