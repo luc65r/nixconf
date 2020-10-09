bbd48a5a272ecde8ca0073906206b006f6e60ca5
Lucas Ransan <lucas@ransan.tk>
2020-10-09 16:00:40 +0200

    Use bépo config

--- desktops/xmonad/xmonad.hs
+++ desktops/xmonad/xmonad.hs
@@ -1,7 +1,8 @@
 import XMonad
+import XMonad.Config.Bepo
 import XMonad.Util.EZConfig
 
-main = xmonad $ def
+main = xmonad $ bepoConfig
     { terminal = "alacritty"
     , modMask = mod4Mask
     } `additionalKeys` [ ((mod4Mask, xK_p), spawn "rofi -show run") ]
