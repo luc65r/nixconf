77ff73781e3a2dea25b54e7bbf17ac744721c26b
Lucas Ransan <lucas@ransan.tk>
2020-10-09 15:43:22 +0200

    use mod4

--- desktops/xmonad/xmonad.hs
+++ desktops/xmonad/xmonad.hs
@@ -4,4 +4,4 @@ import XMonad.Util.EZConfig
 main = xmonad $ def
     { terminal = "alacritty"
     , modMask = mod4Mask
-    } `additionalKeys` [ ((mod1Mask, xK_p), spawn "rofi -show run") ]
+    } `additionalKeys` [ ((mod4Mask, xK_p), spawn "rofi -show run") ]
