585fe860ecccc625acd6bb60e504e6c2ca31d995
Lucas Ransan <lucas@ransan.tk>
2020-10-23 16:44:25 +0200

    enable xmobar

--- desktops/xmonad/home.nix
+++ desktops/xmonad/home.nix
@@ -1,9 +1,17 @@
 { ... }:
 
 {
+  imports = [
+    ../../modules/xmobar.nix
+  ];
+
   xsession.windowManager.xmonad = {
     enable = true;
     enableContribAndExtras = true;
     config = ./xmonad.hs;
+  };
+
+  services.xmobar = {
+    enable = true;
   };
 }
