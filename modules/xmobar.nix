231605e2287b90fa85dd96403fe95b0ee4740431
Lucas Ransan <lucas@ransan.tk>
2020-10-23 16:42:34 +0200

    xmobar module

---
+++ modules/xmobar.nix
@@ -0,0 +1,50 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+
+  cfg = config.services.xmobar;
+
+in {
+  options = {
+    services.xmobar = {
+      enable = mkEnableOption "xmobar status bar";
+
+      config = mkOption {
+        type = types.nullOr types.path;
+        default = null;
+      };
+
+      package = mkOption {
+        type = types.package;
+        default = pkgs.xmobar;
+      };
+    };
+  };
+
+  config = mkIf cfg.enable (mkMerge [
+    {
+      home.packages = [ cfg.package ];
+
+      systemd.user.services.xmobar = {
+        Unit = {
+          Description = "xmobar status bar";
+          After = [ "graphical-session-pre.target" ];
+          PartOf = [ "graphical-session.target" ];
+        };
+
+        Service = {
+          ExecStart = "${cfg.package}/bin/xmobar";
+          Restart = "on-failure";
+        };
+
+        Install = { WantedBy = [ "graphical-session.target" ]; };
+      };
+    }
+
+    (mkIf (cfg.config != null) {
+      xdg.configFile."xmobar/xmobarrc".source = cfg.config;
+    })
+  ]);
+}
