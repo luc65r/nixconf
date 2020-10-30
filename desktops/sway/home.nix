c335c487846d8c4d65892c6c344411fd6f6dbcd7
Lucas Ransan <lucas@ransan.tk>
2020-10-30 14:25:16 +0100

    use bépo in sway

--- desktops/sway/home.nix
+++ desktops/sway/home.nix
@@ -3,5 +3,14 @@
 {
   wayland.windowManager.sway = {
     enable = true;
+    config = {
+      input = {
+        "*" = {
+          xkb_layout = "fr";
+          xkb_variant = "bepo";
+          xkb_options = "ctrl:swapcaps";
+        };
+      };
+    };
   };
 }
