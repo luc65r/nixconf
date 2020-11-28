da5de5561e047c0111c47b4373bef4a3d1174466
Lucas Ransan <lucas@ransan.tk>
2020-11-28 20:55:52 +0100

    use cloudflare dns

--- hosts/flash/default.nix
+++ hosts/flash/default.nix
@@ -60,7 +60,7 @@
     };
     defaultGateway = { address = "192.168.0.1"; interface = "enp4s0"; };
     defaultGateway6 = { address = "2a01:cb19:86ed:f600:46a6:1eff:fe80:c516"; interface = "enp4s0"; };
-    nameservers = [ "84.200.69.80" "84.200.70.40" "2001:1608:10:25::1c04:b12f" "2001:1608:10:25::9249:d69b" ];
+    nameservers = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
   };
 
   sound.enable = true;
