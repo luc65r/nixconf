98d7e4e14b7a8a966b6bc828087890f916fdd53e
Lucas Ransan <lucas@ransan.tk>
2020-11-30 10:03:30 +0100

    add impermanence in flake

--- flake.nix
+++ flake.nix
@@ -5,9 +5,13 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     emacs.url = "github:nix-community/emacs-overlay";
+    impermanence = {
+      url = "github:nix-community/impermanence";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, emacs }: {
+  outputs = { self, nixpkgs, home-manager, emacs, impermanence }: {
     nixosConfigurations = {
       sally = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -41,6 +45,8 @@
             system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
             nix.registry.nixpkgs.flake = nixpkgs;
           }
+
+          (import "${impermanence}/nixos.nix")
 
           home-manager.nixosModules.home-manager
           {
--- hosts/flash/disks.nix
+++ hosts/flash/disks.nix
@@ -3,10 +3,6 @@
 {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
-    "${builtins.fetchTarball {
-      url = "https://github.com/nix-community/impermanence/tarball/4b3000b9bec3a3ce4d5bb7d79abfdd267b5f42ea";
-      sha256 = "0gwx6ggg5gw9w9xnd5k7pq8viknjb88zxn711bqmfcnvnx74hav1";
-    }}/nixos.nix"
   ];
 
   boot = {
