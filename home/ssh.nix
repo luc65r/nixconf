85f837edceaa97152b9ebd011f90ec65df794f6b
Lucas Ransan <lucas@ransan.tk>
2020-11-30 15:18:13 +0100

    add keygrip

--- home/ssh.nix
+++ home/ssh.nix
@@ -9,8 +9,17 @@
     enable = true;
   };
 
-  services.gpg-agent = {
+  services.gpg-agent = rec {
     enable = true;
     enableSshSupport = true;
+
+    sshKeys = [
+      "CF3A5BE1AE37A59C2B06FFE27EF6C6758269833E"
+    ];
+
+    defaultCacheTtl = 1800;
+    maxCacheTtl = 8 * defaultCacheTtl;
+    defaultCacheTtlSsh = 1800;
+    maxCacheTtlSsh = 8 * defaultCacheTtlSsh;
   };
 }
