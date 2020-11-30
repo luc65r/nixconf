3ac4e6031b346735a284e0217c9dbcdbaea62f01
Lucas Ransan <lucas@ransan.tk>
2020-11-30 14:47:29 +0100

    add gpg signing in git
    Signed-off-by: Lucas Ransan <lucas@ransan.tk>

--- home/git.nix
+++ home/git.nix
@@ -8,6 +8,11 @@
     userName = "Lucas Ransan";
     userEmail = "lucas@ransan.tk";
 
+    signing = {
+      key = "0x37E8293E1B8B2307";
+      signByDefault = true;
+    };
+
     ignores = [ "*~" ];
 
     extraConfig = {
