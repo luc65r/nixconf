f74239effab3a7d7ae637ffe2618bed764cf2217
Lucas Ransan <lucas@ransan.tk>
2020-12-01 09:00:54 +0100

    add vim config

--- home/editor.nix
+++ home/editor.nix
@@ -7,8 +7,19 @@
     plugins = with pkgs.vimPlugins; [
       vim-repeat
       vim-surround
-      vim-polyglot
+      { plugin = haskell-vim;
+        config = "let g:haskell_indent_guard = 4";
+      }
     ];
+
+    extraConfig = ''
+      " Turn of the ugly status line
+      set laststatus=0
+
+      command! W execute 'w !sudo tee % > /dev/null' <bar> edit!
+
+      set ts=4 sts=4 sw=4 et
+    '';
   };
 
   programs.emacs = {
