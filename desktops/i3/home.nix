2b5e033a54650bb0ed29518140485febd8258509
Lucas Ransan <lucas@ransan.tk>
2020-12-01 18:24:42 +0100

    add basic polybar

--- desktops/i3/home.nix
+++ desktops/i3/home.nix
@@ -16,6 +16,46 @@
     };
   };
 
+  services.polybar = {
+    enable = true;
+
+    package = pkgs.polybar.override {
+      i3Support = true;
+      pulseSupport = true;
+      mpdSupport = true;
+    };
+
+    config = {
+      "bar/top" = {
+        bottom = false;
+        modules-left = "i3";
+        modules-center = "mpd";
+        modules-right = "volume date";
+        tray-position = "right";
+      };
+
+      "module/i3" = {
+        type = "internal/i3";
+      };
+
+      "module/date" = {
+        type = "internal/date";
+      };
+
+      "module/volume" = {
+        type = "internal/volume";
+      };
+
+      "module/mpd" = {
+        type = "internal/mpd";
+        host = "127.0.0.1";
+        port = "6600";
+      };
+    };
+
+    script = "polybar top &";
+  };
+
   home.packages = with pkgs; [
     rofi
   ];
