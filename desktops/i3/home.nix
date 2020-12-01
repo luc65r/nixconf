a29e096dca4cc84bc89d6702aded713209593481
Lucas Ransan <lucas@ransan.tk>
2020-12-01 16:16:36 +0100

    use rofi in i3

--- desktops/i3/home.nix
+++ desktops/i3/home.nix
@@ -1,11 +1,22 @@
-{ ... }:
+{ lib, pkgs, ... }:
 
 {
   xsession.windowManager.i3 = {
     enable = true;
-    config = {
+    config = rec {
       terminal = "alacritty";
       modifier = "Mod4";
+      menu = "rofi -show drun";
+
+      keybindings = lib.mkOptionDefault {
+        "${modifier}+q" = "kill";
+      };
+
+      bars = [];
     };
   };
+
+  home.packages = with pkgs; [
+    rofi
+  ];
 }
