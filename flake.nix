149ebbb0e2401cec1eaf8e160db083e1f57f5d90
Lucas Ransan <lucas@ransan.tk>
2020-12-05 17:07:43 +0100

    switch to master

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/master";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
