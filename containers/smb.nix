c64f0a710dd15356c9a328c69de1b6b29307dbdd
Lucas Ransan <lucas@ransan.tk>
2021-03-18 00:19:49 +0100

    open everything, fuck security

--- containers/smb.nix
+++ containers/smb.nix
@@ -3,13 +3,23 @@
 {
   services.samba = {
     enable = true;
+    extraConfig = ''
+      guest account = nobody
+      map to guest = bad user
+    '';
     shares = {
       torrent = {
         path = "/srv/torrent/Downloads";
         browsable = true;
         "read only" = true;
         "guest ok" = true;
+        "public" = true;
       };
     };
+  };
+
+  networking.firewall = {
+    allowedTCPPorts = [ 445 139 ];
+    allowedUDPPorts = [ 137 138 ];
   };
 }
