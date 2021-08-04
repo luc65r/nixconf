e9d28c6f61448be3f208cf7831298c9952e63aa0
Lucas Ransan <lucas@ransan.tk>
2021-08-04 20:20:30 +0200

    make flash use nixos-21.05

--- flake.nix
+++ flake.nix
@@ -2,10 +2,11 @@
   description = "My NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-21.05";
     home-manager = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
     emacs.url = "github:nix-community/emacs-overlay";
     impermanence = {
@@ -15,23 +16,24 @@
     flake-utils.url = "github:numtide/flake-utils";
     secrets = {
       url = "git+file:///home/lucas/nixsecrets";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
     cyrel = {
       url = "git+ssh://git@github.com/Cyrel-org/cyrel-functions";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
       inputs.flake-utils.follows = "flake-utils";
     };
     botCYeste = {
       url = "github:luc65r/botCYeste";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
       inputs.flake-utils.follows = "flake-utils";
     };
   };
 
   outputs =
     { self
-    , nixpkgs
+    , nixpkgs-unstable
+    , nixpkgs-stable
     , home-manager
     , emacs
     , impermanence
@@ -41,72 +43,81 @@
     , botCYeste
     }: {
       nixosConfigurations = let
-        defaultConfig = name: {
-          system = "x86_64-linux";
+        defaultConfig =
+          { name
+          , nixpkgs
+          }: {
+            system = "x86_64-linux";
 
-          specialArgs = {
-            host = {
-              inherit name;
-              type = "desktop";
-              keymap = "bepo";
-              wm = "sway";
+            specialArgs = {
+              host = {
+                inherit name;
+                type = "desktop";
+                keymap = "bepo";
+                wm = "sway";
+              };
+
+              inherit (secrets) secrets;
             };
 
-            inherit (secrets) secrets;
+            modules = [
+              (./hosts + "/${name}")
+              {
+                system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
+                nix.registry.nixpkgs.flake = nixpkgs;
+              }
+
+              (import "${impermanence}/nixos.nix")
+
+              home-manager.nixosModules.home-manager
+              ({ host, secrets, ... }: {
+                options.home-manager.users = with nixpkgs.lib; mkOption {
+                  type = with types; attrsOf (submoduleWith {
+                    modules = [ ];
+                    specialArgs = {
+                      inherit host secrets;
+                    };
+                  });
+                };
+              })
+              {
+                home-manager = {
+                  useGlobalPkgs = true;
+                  useUserPackages = true;
+                  users.lucas = import ./home;
+                };
+              }
+
+              {
+                nixpkgs.overlays = [
+                  emacs.overlay
+
+                  (_: _: {
+                    cyrel = cyrel.defaultPackage."x86_64-linux";
+                    botCYeste = botCYeste.defaultPackage."x86_64-linux";
+                  })
+                ];
+              }
+            ];
           };
-
-          modules = [
-            (./hosts + "/${name}")
-            {
-              system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
-              nix.registry.nixpkgs.flake = nixpkgs;
-            }
-
-            (import "${impermanence}/nixos.nix")
-
-            home-manager.nixosModules.home-manager
-            ({ host, secrets, ... }: {
-              options.home-manager.users = with nixpkgs.lib; mkOption {
-                type = with types; attrsOf (submoduleWith {
-                  modules = [ ];
-                  specialArgs = {
-                    inherit host secrets;
-                  };
-                });
-              };
-            })
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                users.lucas = import ./home;
-              };
-            }
-
-            {
-              nixpkgs.overlays = [
-                emacs.overlay
-
-                (_: _: {
-                  cyrel = cyrel.defaultPackage."x86_64-linux";
-                  botCYeste = botCYeste.defaultPackage."x86_64-linux";
-                })
-              ];
-            }
-          ];
-        };
       in {
-        sally = nixpkgs.lib.nixosSystem
-          (nixpkgs.lib.recursiveUpdate (defaultConfig "sally") {
+        sally = nixpkgs-unstable.lib.nixosSystem
+          (nixpkgs-unstable.lib.recursiveUpdate (defaultConfig {
+            name = "sally";
+            nixpkgs = nixpkgs-unstable;
+          }) {
             specialArgs.host.type = "laptop";
           });
 
-        flash = nixpkgs.lib.nixosSystem
-          (nixpkgs.lib.recursiveUpdate (defaultConfig "flash") {
+        flash = nixpkgs-stable.lib.nixosSystem
+          (nixpkgs-stable.lib.recursiveUpdate (defaultConfig {
+            name = "flash";
+            nixpkgs = nixpkgs-stable;
+          }) {
           });
       };
     } // flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = nixpkgs-unstable.legacyPackages.${system};
     in {
       devShell = pkgs.mkShell {
         nativeBuildInputs = with pkgs; [
