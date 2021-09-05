0fd613e80206c4e1124a040adf6d60744ce45eee
Lucas Ransan <lucas@ransan.tk>
2021-09-05 19:50:47 +0200

    Revert "fix sublime-music"
    This reverts commit 466730ef9e9067c05605f7602df8513a376d0c77.

--- home/music.nix
+++ home/music.nix
@@ -20,14 +20,6 @@
 
   home.packages = with pkgs; [
     mpc_cli
-    (sublime-music.overrideAttrs (old: rec {
-      version = "0.11.13";
-      src = pkgs.fetchFromGitLab {
-        owner = "sublime-music";
-        repo = "sublime-music";
-        rev = "v0.11.13";
-        sha256 = "NzbQtRcsRVppyuG1UuS3IidSnniUOavf5YoAf/kcZqw=";
-      };
-    }))
+    sublime-music
   ];
 }
