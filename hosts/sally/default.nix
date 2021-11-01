bbdbafcc866277212bcf8f6f5a238397d996e1df
Lucas Ransan <lucas@ransan.tk>
2021-11-01 22:16:29 +0100

    enable jack and 32 bit to pipewire

--- hosts/sally/default.nix
+++ hosts/sally/default.nix
@@ -87,7 +87,9 @@
   services.pipewire = {
     enable = true;
     alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
+    jack.enable = true;
     media-session = {
       config.bluez-monitor = {
         properties.bluez5 = {
