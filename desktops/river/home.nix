a8ea7427b76fe0c7d507785fd80fd35b2f8c82bf
Lucas Ransan <lucas@ransan.tk>
2021-12-19 22:30:30 +0100

    fix waybar on river

--- desktops/river/home.nix
+++ desktops/river/home.nix
@@ -3,6 +3,14 @@
 {
   programs.waybar = {
     enable = true;
+    package = pkgs.waybar.overrideAttrs (old: {
+      patches = [
+        (pkgs.fetchpatch {
+          url = "https://github.com/Alexays/Waybar/commit/024fd42e272f79cb08e594eb3af86c93b1446c91.patch";
+          sha256 = "NExAWhuizKiyYgAPNIUPucGz5GDi8umevAOY5HtLQJk=";
+        })
+      ];
+    });
     systemd.enable = true;
 
     settings = [{
