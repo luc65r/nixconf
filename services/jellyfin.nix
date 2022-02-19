e65fea17c1baf10047d349d1d795cd621d5cfab5
Lucas Ransan <lucas@ransan.tk>
2022-02-19 18:08:36 +0100

    fix nvenc on jellyfin

--- services/jellyfin.nix
+++ services/jellyfin.nix
@@ -1,8 +1,14 @@
-{ ... }:
+{ lib, ... }:
 
 {
   services.jellyfin = {
     enable = true;
+  };
+
+  systemd.services.jellyfin.serviceConfig = {
+    DeviceAllow = lib.mkForce [ "char-drm rw" "char-nvidia-frontend rw" "char-nvidia-uvm rw" ];
+    PrivateDevices = lib.mkForce false;
+    RestrictAddressFamilies = lib.mkForce [ "AF_UNIX" "AF_NETLINK" "AF_INET" "AF_INET6" ];
   };
 
   services.nginx.virtualHosts."jellyfin.ransan.tk" = {
